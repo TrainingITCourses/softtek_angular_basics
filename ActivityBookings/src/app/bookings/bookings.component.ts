0d33cc628ecbe5f9a85d88e5afd1b977fed5fac5
Alberto Basalo <albertobasalo@hotmail.com>
2024-02-28 17:12:19 +0100

    bookings component

---
+++ ActivityBookings/src/app/bookings/bookings.component.ts
@@ -0,0 +1,57 @@
+import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Activity } from '../domain/activity.type';
+
+@Component({
+  selector: 'lab-bookings',
+  standalone: true,
+  imports: [CurrencyPipe, DatePipe, UpperCasePipe],
+  template: `
+    <h2>{{ activity.name }}</h2>
+    <p [class]="activity.status">
+      {{ activity.location }} {{ activity.price | currency }}
+      {{ activity.date | date : 'dd-MMM-yyyy' }}
+      {{ activity.status | uppercase }}
+    </p>
+  `,
+  styles: `
+    .draft {
+      color: violet;
+      font-style: italic;
+    }
+    .published {
+      color: limegreen;
+    }
+    .confirmed {
+      color: green;
+    }
+    .sold-out {
+      color: green;
+      font-style: italic;
+    }
+    .done {
+      color: orange;
+      font-style: italic;
+    }
+    .cancelled {
+      color: red;
+      font-style: italic;
+    }
+`,
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class BookingsComponent {
+  activity: Activity = {
+    name: 'Paddle surf',
+    location: 'Lake Leman at Lausanne',
+    price: 100,
+    date: new Date(2025, 7, 15),
+    minParticipants: 4,
+    maxParticipants: 10,
+    status: 'confirmed',
+    id: 1,
+    slug: 'paddle-surf',
+    duration: 2,
+    userId: 1,
+  };
+}
